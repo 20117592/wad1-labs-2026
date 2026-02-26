d2045aaaf9c09d7952335398f8e0459e090fa6a0
Noah Dempsey <noahdemc64@Noahs-MacBook-Pro.local>
2026-02-26 12:41:46 +0000

    Changes now being deployed to GitHub and Render

--- controllers/start.js
+++ controllers/start.js
@@ -4,7 +4,7 @@ import logger from "../utils/logger.js";
 const start = {
   createView(request, response) {
     logger.info("Start page loading!");
-    response.send('Welcome to Noah's Playlist app!');   
+    response.send('Welcome to Noahs Playlist app!');   
   },
 };
 
