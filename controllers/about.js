67d72bf2fee50c5b635b2133d4197ac85a2d8353
Noah Dempsey <noahdemc64@MacBookPro.fritz.box>
2026-03-07 23:26:01 +0000

    Add Models and Views

--- controllers/about.js
+++ controllers/about.js
@@ -1,16 +1,17 @@
 'use strict';
 import logger from "../utils/logger.js";
+import hhh from "../models/hhh.js";
 const about = {
   createView(request, response) {
     logger.info("About page loading!");
 
     const viewData={
-      title: "About the Playlist App"
+      title: "About the Playlist App",
+      employee: hhh.getAppInfo()[0]
     };
     
     response.render('about', viewData);   
   },
 };
-
 
 export default about;
